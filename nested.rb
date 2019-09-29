51aa7e6f429cc82d35feb2c2a9fc03eaf8fdb308
Aniece Thompson <aniecet@yahoo.com>
2019-09-29 05:53:26 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,7 +81,6 @@ def adding_matz
         }
      }
   programmer_hash[:yukihiro_matsumoto] = {:known_for{}, :Languages}
-  programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
   
   
   programmer_hash
